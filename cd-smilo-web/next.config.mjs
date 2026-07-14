73878b411d62b17f1652e267bf7633e2840011d7
Jorge Rubio <jrubiod90@gmail.com>
2026-07-14 07:30:45 +0200

    Changes to deploy in github pages

--- cd-smilo-web/next.config.mjs
+++ cd-smilo-web/next.config.mjs
@@ -1,5 +1,12 @@
+const isGithubPages = process.env.GITHUB_PAGES === 'true'
+const repoName = 'cdSmiloWeb'
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+  trailingSlash: true,
+  basePath: isGithubPages ? `/${repoName}` : '',
+  assetPrefix: isGithubPages ? `/${repoName}/` : '',
   images: {
     unoptimized: true,
   },
